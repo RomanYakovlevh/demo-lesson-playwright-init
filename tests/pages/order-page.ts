704643363e645a8b4b88437d9140b5179429ebe5
romanyakovlev <ryakovlev@evolution.com>
2025-07-14 14:36:25 +0300

    comit

--- tests/pages/order-page.ts
+++ tests/pages/order-page.ts
@@ -3,9 +3,23 @@ import { Locator, Page } from '@playwright/test'
 export class OrderPage {
   readonly page: Page
   readonly statusButton: Locator
+  readonly clientName: Locator
+  readonly clientPhone:Locator
+  readonly createOrderButton: Locator
+  readonly okButton: Locator
 
   constructor(page: Page) {
     this.page = page
     this.statusButton = page.getByTestId('openStatusPopup-button')
+    this.clientName = page.getByTestId('username-input')
+    this.clientPhone = page.getByTestId('phone-input')
+    this.createOrderButton = page.getByTestId('createOrder-button')
+    this.okButton = page.getByTestId('orderSuccessfullyCreated-popup-ok-button')
+  }
+
+  async createOrder() {
+    await this.clientName.fill('Random A. Name')
+    await this.clientPhone.fill('123456789')
+    await this.createOrderButton.click()
   }
 }
--- tests/ui/auth/auth.spec.ts
+++ tests/ui/auth/auth.spec.ts
@@ -1,10 +1,44 @@
 import { expect, test } from '@playwright/test'
 import { SERVICE_URL } from '../../../config/env-data'
+import assert from 'node:assert'
 
-test('Sign in button is disabled when an invalid username is entered', async ({ page }) => {
+test('test 1', async ({ page }) => {
   await page.goto(SERVICE_URL)
   const usernameField = page.getByTestId('username-input')
   await usernameField.fill('-')
   const signInButton = page.getByTestId('signIn-button')
+  const usernameError = page.getByTestId('username-input-error').nth(0)
   await expect(signInButton).toBeDisabled()
+  await expect(usernameError).toBeVisible()
+})
+
+test('test 2', async ({ page }) => {
+  await page.goto(SERVICE_URL)
+  const usernameField = page.getByTestId('username-input')
+  await usernameField.fill('askjdbavas')
+  const passwordField = page.getByTestId('password-input')
+  await passwordField.fill("sjkaskdhjkdjajksaj")
+  const signInButton = page.getByTestId('signIn-button')
+  await signInButton.click()
+  const loginError = page.getByTestId('authorizationError-popup')
+  await expect(loginError).toBeVisible()
+})
+
+test('test 3', async ({ page }) => {
+  await page.goto(SERVICE_URL)
+  const usernameField = page.getByTestId('username-input')
+  await usernameField.fill('evolution-19')
+  const passwordField = page.getByTestId('password-input')
+  await passwordField.fill("uP9nZtLo")
+  const signInButton = page.getByTestId('signIn-button')
+  await signInButton.click()
+  const createOrder = page.getByText('Create orderOrder×Order has')
+  await expect(createOrder).toBeVisible()
+})
+
+test('completely different website', async ({ page }) => {
+  await page.goto('https://www.huurwoningen.nl/')
+  const login = page.getByText('Login')
+  await expect(login).toBeVisible()
+
 })
